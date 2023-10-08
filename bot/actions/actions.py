cc8666cc379099561fd664c97d3eeabd25df88e5
Victor Eduardo <victorear05@gmail.com>
2023-10-08 15:33:27 -0300

    (#75) - Refatorar test_stories.yml

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -48,22 +48,3 @@ class ActionDefaultFallback(Action):
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         dispatcher.utter_message(template="utter_core_fallback")
         return [UserUtteranceReverted()]
-
-# Ações Personalizadas
-class ActionUtterFontesConfiaveis(Action):
-    def name(self) -> Text:
-        return "action_utter_fontes_confiaveis"
-
-    async def run(
-        self, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker, 
-        domain):
-
-        dispatcher.utter_message(text="Teste Nesta funcionalidade, você terá acesso a uma variedade de fontes confiáveis para suas pesquisas. Explore as melhores bases acadêmicas para conduzir suas investigações!", parse_mode='Markdown')
-        dispatcher.utter_message(text="**Selecione o seu estágio de pesquisa:**", parse_mode='Markdown')
-        dispatcher.utter_message(text="1. **Exploração Inicial:** Acesse bases acadêmicas de fácil acesso com conteúdo geral, perfeitas para iniciantes em pesquisas (/iniciante).", parse_mode='Markdown')
-        dispatcher.utter_message(text="2. **Navegando pelo Conhecimento:** Descubra bases acadêmicas detalhadas disponíveis para estudantes que desejam aprofundar suas pesquisas (/navegador).", parse_mode='Markdown')
-        dispatcher.utter_message(text="3. **Explorando Conteúdos Avançados:** Explore bases acadêmicas altamente especializadas e detalhadas, que podem ter restrições de acesso. Este desafio é ideal para pesquisadores experientes que desejam se especializar ainda mais (/explorador).", parse_mode='Markdown')
-
-        return [UserUtteranceReverted()]
