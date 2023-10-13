ade3b096663889498a9bc74ab54852d2bf759601
Victor Eduardo <victorear05@gmail.com>
2023-10-13 16:39:53 -0300

    (#79) - Pontuação Rasa Test

--- bot/actions/actions_default.py
+++ bot/actions/actions_default.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -18,14 +17,10 @@ class ActionSessionStart(Action):
       self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
     ) -> List[Dict[Text, Any]]:
         
-        logging.basicConfig(level=logging.DEBUG)
-
         sender_id = tracker.sender_id
         
         with SessionLocal() as session:
             user = session.query(UserInfo).filter_by(id=sender_id).first()
-            logging.warning("NOT USER")
-            logging.warning(sender_id)
             if not user:
                 new_user = UserInfo(id=sender_id)
                 session.add(new_user)
